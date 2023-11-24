fe102f83216ec2dcd655894d0ab992eebfaa45f2
Fernando Freitas <freitax.li@gmail.com>
2023-11-24 12:18:06 -0300

    Arrumar interface e criar janela Sobre.fxml

--- src/main/java/br/com/ifsp/nando/gerenciadortarefasescolares/Main.java
+++ src/main/java/br/com/ifsp/nando/gerenciadortarefasescolares/Main.java
@@ -4,7 +4,7 @@ import br.com.ifsp.nando.gerenciadortarefasescolares.view.GerenciadorTarefasEsco
 
 public class Main {
 
-    public static final String VERSAO_GERENCIADOR = "0.8-alpha";
+    public static final String VERSAO_GERENCIADOR = "1.0.0";
 
     public static void main(String[] args) {
         GerenciadorTarefasEscolares.main(args);
